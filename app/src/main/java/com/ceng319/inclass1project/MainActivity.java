3343e1f53569e09a0ba1fdacd2a47d5a31b5985e
BW25 <41970894+BW25@users.noreply.github.com>
2019-09-18 13:45:07 -0400

    Adding sample code

--- app/src/main/java/com/ceng319/inclass1project/MainActivity.java
+++ app/src/main/java/com/ceng319/inclass1project/MainActivity.java
@@ -3,6 +3,9 @@ package com.ceng319.inclass1project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello, this is my first Android APP", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
